b9e3361b2d6ffdddf2c3d261266fbf95df6ffa25
Devin <devinrolfe@gmail.com>
2015-08-15 18:49:05 -0400

    Did basic layout of app

--- app/src/main/java/com/nullnothing/relationshipstats/RelationshipStatsActivity.java
+++ app/src/main/java/com/nullnothing/relationshipstats/RelationshipStatsActivity.java
@@ -2,17 +2,30 @@ package com.nullnothing.relationshipstats;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class RelationshipStatsActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relationship_stats);
+
+        // Set a Toolbar to replace the ActionBar.
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Find our drawer view
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         // Get the ViewPager and set it's PagerAdapter so that it can display items
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
@@ -35,16 +48,30 @@ public class RelationshipStatsActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_search:
-//                openSearch();
+                openSearch();
                 return true;
             case R.id.action_add_contact:
-//               openAddContact();
+               openAddContact();
                 return true;
             case R.id.action_option:
-//                openSettings();
+                mDrawer.openDrawer(GravityCompat.END);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    public void openSearch() {
+
+
+    }
+
+    public void openAddContact() {
+
+    }
+
+
+
+
+
 }
