3311f87cd009c8ad71092d2fa9f9fddd4ea8ee4c
Devin <devinrolfe@gmail.com>
2015-08-16 11:01:33 -0400

    completed code to get raw contact messages. Need to now start creating an object to hold on all the information

--- app/src/main/java/com/nullnothing/relationshipstats/RawDataFragment.java
+++ app/src/main/java/com/nullnothing/relationshipstats/RawDataFragment.java
@@ -1,5 +1,6 @@
 package com.nullnothing.relationshipstats;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,17 +12,47 @@ import android.widget.TextView;
 
 // In this case, the fragment displays simple text based on the page
 public class RawDataFragment extends Fragment {
+    RawDataSelectedListener mCallback;
+
+    private TextView textMessageView;
+
+    // Container Activity must implement this interface
+    public interface RawDataSelectedListener {
+        public void getNextText(View view);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mCallback = (RawDataSelectedListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement RawDataSelectedListener");
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_raw_data, container, false);
-
         FrameLayout frameLayout = (FrameLayout) view;
-
-        TextView textView = (TextView) frameLayout.getChildAt(0);
-        textView.setText("Fragment: Raw Data");
+        textMessageView = (TextView) view.findViewById(R.id.fragment_raw_data_text_view);
+        textMessageView.setText(this.getTag());
         return view;
     }
+
+    // Get the next text message and display it
+    public void getNextTextFromActivity(String message) {
+        if(message != null) {
+            textMessageView.setText(message);
+        }else {
+            textMessageView.setText("FINISHED");
+        }
+    }
+
 }
 
 
--- app/src/main/java/com/nullnothing/relationshipstats/RelationshipStatsActivity.java
+++ app/src/main/java/com/nullnothing/relationshipstats/RelationshipStatsActivity.java
@@ -1,19 +1,28 @@
 package com.nullnothing.relationshipstats;
 
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-public class RelationshipStatsActivity extends AppCompatActivity {
+public class RelationshipStatsActivity extends AppCompatActivity
+        implements RawDataFragment.RawDataSelectedListener {
 
     private DrawerLayout mDrawer;
     private Toolbar toolbar;
+    private ViewPager viewPager;
+    private Cursor cur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +37,17 @@ public class RelationshipStatsActivity extends AppCompatActivity {
         mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         // Get the ViewPager and set it's PagerAdapter so that it can display items
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(new RelationshipStatsFragmentPagerAdapter(getSupportFragmentManager(), RelationshipStatsActivity.this));
 
         // Give the TabLayout the ViewPager
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+        // use "content://sms/sent" to get text messages you sent
+        Uri uriSMSURI = Uri.parse("content://sms/inbox");
+        cur = getContentResolver().query(uriSMSURI, null, null, null, null);
+
+
 
     }
 
@@ -69,6 +83,44 @@ public class RelationshipStatsActivity extends AppCompatActivity {
 
     }
 
+    // Get next text message in sms logs
+    public void getNextText(View view) {
+        TextView textView = (TextView) findViewById(R.id.fragment_raw_data_text_view);
+
+        String sms = "";
+        if (cur.moveToNext()) {
+            sms += "From :" + cur.getString(2) + " : " + cur.getString(cur.getColumnIndex("body"))+"\n";
+            //return text message to fragment
+        }
+
+        RawDataFragment rawDataFrag = (RawDataFragment) getSupportFragmentManager().findFragmentByTag(
+                "android:switcher:" + R.id.viewpager + ":" + viewPager.getCurrentItem());
+
+        if (rawDataFrag != null) {
+            rawDataFrag.getNextTextFromActivity(sms);
+        }
+        else{
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(RelationshipStatsActivity.this);
+            builder1.setMessage("Error: Something went wrong");
+            builder1.setCancelable(true);
+            builder1.setPositiveButton("Yes",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            builder1.setNegativeButton("No",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+        }
+
+    }
 
 
 }
