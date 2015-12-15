72f80fa4d99cbc403908b047ebac8280b4db73ba
devindrolfe <devinrolfe@gmail.com>
2015-12-15 22:28:59 +0000

    Added Calender variable in TextMessages
    The calender variable will be used to put the message in the correct dataset when graphing
    the message

--- TextStats/src/main/java/com/nullnothing/relationshipstats/TextMessage.java
+++ TextStats/src/main/java/com/nullnothing/relationshipstats/TextMessage.java
@@ -3,25 +3,34 @@ package com.nullnothing.relationshipstats;
 
 import com.nullnothing.relationshipstats.Enums.TimePeriod;
 
+import java.util.Calendar;
+
 public abstract class TextMessage {
 
     private long timestamp;
     private String message;
-    private int wordCount;
+    private Calendar cal = Calendar.getInstance();
 
     public TextMessage() {
-        // User should never call this constructer... ideally this class should of been interface
+        // Would like to make this constructer not instainateable but can't because of decorator..
+//        throw new AssertionError();
     }
 
     public TextMessage(long timestamp, String message) {
         this.timestamp = timestamp;
         this.message = message;
-        this.wordCount = 1;
+        cal.setTimeInMillis(System.currentTimeMillis());
     }
 
     public String getMessage() { return this.message; }
     public long getTimestamp() { return this.timestamp; }
 
     public TimePeriod getPeriod() { return TimePeriod.ALL_TIME; }
+
+    public int getYear() { return cal.get(Calendar.YEAR);}
+    public int getMonth() { return cal.get(Calendar.MONTH);}
+    public int getDay() { return cal.get(Calendar.DATE);}
+    public int getHour() { return cal.get(Calendar.AM_PM);}
+    public int getMinute() { return cal.get(Calendar.MINUTE);}
 
 }
